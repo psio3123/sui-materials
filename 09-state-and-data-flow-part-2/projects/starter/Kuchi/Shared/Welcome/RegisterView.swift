e77ad8ec83f17d097d67b17c852b4c1eb790dc91
Harald Bucher <harald.bucher@posteo.de>
2022-12-28 23:09:16 +0100

    End of chapter 9

--- 09-state-and-data-flow-part-2/projects/starter/Kuchi/Shared/Welcome/RegisterView.swift
+++ 09-state-and-data-flow-part-2/projects/starter/Kuchi/Shared/Welcome/RegisterView.swift
@@ -1,37 +1,6 @@
-/// Copyright (c) 2021 Razeware LLC
-///
-/// Permission is hereby granted, free of charge, to any person obtaining a copy
-/// of this software and associated documentation files (the "Software"), to deal
-/// in the Software without restriction, including without limitation the rights
-/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-/// copies of the Software, and to permit persons to whom the Software is
-/// furnished to do so, subject to the following conditions:
-///
-/// The above copyright notice and this permission notice shall be included in
-/// all copies or substantial portions of the Software.
-///
-/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
-/// distribute, sublicense, create a derivative work, and/or sell copies of the
-/// Software in any work that is designed, intended, or marketed for pedagogical or
-/// instructional purposes related to programming, coding, application development,
-/// or information technology.  Permission for such use, copying, modification,
-/// merger, publication, distribution, sublicensing, creation of derivative works,
-/// or sale is expressly withheld.
-///
-/// This project and source code may use libraries or frameworks that are
-/// released under various Open-Source licenses. Use of those libraries and
-/// frameworks are governed by their own individual licenses.
-///
-/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-/// THE SOFTWARE.
-
 import SwiftUI
 
+@available(macOS 12.0, *)
 struct RegisterView: View {
   @EnvironmentObject var userManager: UserManager
   @FocusState var nameFieldFocused: Bool
@@ -90,6 +59,7 @@ struct RegisterView: View {
 }
 
 // MARK: - Event Handlers
+@available(macOS 12.0, *)
 extension RegisterView {
   func registerUser() {
     nameFieldFocused = false
@@ -109,7 +79,11 @@ struct RegisterView_Previews: PreviewProvider {
   static let user = UserManager(name: "Ray")
 
   static var previews: some View {
-    RegisterView()
-      .environmentObject(user)
+      if #available(macOS 12.0, *) {
+          RegisterView()
+              .environmentObject(user)
+      } else {
+          // Fallback on earlier versions
+      }
   }
 }
